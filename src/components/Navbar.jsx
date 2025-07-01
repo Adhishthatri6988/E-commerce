de443d9a48a671b8e689f43a382578a9a9910cb8
Adhishthatri <asunasingh2003@gmail.com>
2025-07-01 21:25:49 +0530

    mobile screen menu bar and back button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import { useState} from 'react'
 import {assets} from "../assets/frontend_assets/assets.js"
 import { Link, NavLink } from 'react-router-dom'
 
 
 const NavBar = () => {
+  const [visible, setVisible] = useState(false);
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
       <img src={assets.logo} alt="Logo" className='w-36' />
@@ -44,11 +45,22 @@ const NavBar = () => {
           <Link to={"/cart"} className='relative'>
             <img src={assets.cart_icon} alt="Cart" className='w-5 min-w-5 cursor-pointer' />
             <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>0</p>
-            
           </Link>
-
+          <img onClick={()=> setVisible(true)} src={assets.menu_icon} alt="Menu" className='w-5 cursor-pointer sm:hidden' />
 
         </div>
+
+      {/*sidebar menu for mobile view */}
+      <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible?'w-full': 'w-0'}`}>
+          <div className='flex flex-col text-gray-600 '>
+            <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3'>
+              <img src={assets.dropdown_icon} className='h-4 rotate-180'/>
+              <p>Back</p>
+            </div>
+          </div>
+      </div>
+
+
       </div>
   )
 }

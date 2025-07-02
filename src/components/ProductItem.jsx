c9955552200539c1080f3d5e663a0b1190735b28
Adhishthatri <asunasingh2003@gmail.com>
2025-07-02 17:57:28 +0530

    solved error in latest collection and product item

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+
+import { useContext } from 'react';
+import { ShopContext } from '../context/ShopContext';
 import { Link } from 'react-router-dom';
 
-const ProductItem = ({id , name , img , price}) => {
+const ProductItem = ({id , name , image , price}) => {
   const {currency} = useContext(ShopContext);
 
   return (
     <Link to={`/product/${id}`} className='text-gray-700 cursor-pointer'>
       <div className=' overflow-hidden'>
-        <img src={Image[0]} alt="" className='transition ease-in-out hover:scale-115' />
+        <img src={image[0]} alt="" className='transition ease-in-out hover:scale-115' />
       </div>
       <p className='pt-3 pb-1 text-sm'>{name}</p>
       <p className='text-sm font-medium'>{currency} {price}</p>

31262f5158eb8a9c86aa0e0cd963e5e0d4d19586
Adhishthatri <asunasingh2003@gmail.com>
2025-07-02 16:46:30 +0530

    created ShopContextProvider

---
+++ src/context/ShopContext.jsx
@@ -0,0 +1,20 @@
+import { createContext } from 'react';
+import { products } from '../assets/frontend_assets/assets';
+export const ShopContext = createContext(null);
+
+const ShopContextProvider = (props) => {
+
+  const currency = "$"; 
+  const delivery_fee = 10;
+  const Value = {
+    // Define your context values here
+    products , currency, delivery_fee,
+  };
+  return (
+    <ShopContext.Provider value={Value}>
+      {props.children}
+    </ShopContext.Provider>
+  );
+}
+
+export default ShopContextProvider;

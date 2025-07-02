ae92318ff27fdf0de446cb7113d957122ddf5bad
Adhishthatri <asunasingh2003@gmail.com>
2025-07-02 19:48:55 +0530

    added best seller section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,9 @@ const NavBar = () => {
   const [visible, setVisible] = useState(false);
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
+      <Link to='/'>
       <img src={assets.logo} alt="Logo" className='w-36' />
+      </Link>
         <ul className='hidden sm:flex gap-5 text-5m text-gray-700'>
         <NavLink to='/' className='flex flex-col items-center gap-1'>
         <p>HOME</p>

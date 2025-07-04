e729ebe2a05e4ccb07710fc849e602dc171c2394
Adhishthatri <asunasingh2003@gmail.com>
2025-07-04 20:35:49 +0530

    modified shopcontext.jsx for search functionalities

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -6,9 +6,13 @@ const ShopContextProvider = (props) => {
 
   const currency = "$"; 
   const delivery_fee = 10;
+
+  const [search , setSearch] = useState();
+  const [showSearch , setShowSearch] = useState(true);
   const Value = {
     // Define your context values here
     products , currency, delivery_fee,
+    search, setSearch, showSearch, setShowSearch
   };
   return (
     <ShopContext.Provider value={Value}>

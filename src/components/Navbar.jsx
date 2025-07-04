2d6b1a2501b0c31d1eeec280c10c183b49e62901
Adhishthatri <asunasingh2003@gmail.com>
2025-07-04 19:16:40 +0530

    fixed error in navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const NavBar = () => {
         <p>HOME</p>
         <hr className='w-2/4 boredr-none h-[1.5px] bg-gray-700 hidden'></hr>
         </NavLink>
-        <NavLink to='/connection' className='flex flex-col items-center gap-1 '>
+        <NavLink to='/collection' className='flex flex-col items-center gap-1 '>
         <p>COLLECTION</p>
         <hr className='w-2/4 boredr-none h-[1.5px] bg-gray-700 hidden'></hr>
         </NavLink>

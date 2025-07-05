0e606d2ba03ecd12e5d835db290eda0bfb124e76
Adhishthatri <asunasingh2003@gmail.com>
2025-07-05 21:10:02 +0530

    add to cart , size , policy got added

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -8,6 +8,7 @@ const Product = () => {
   const {products , currency} = useContext(ShopContext);
   const [productData , setProductData] = useState(false);
   const [image , setImage] = useState('');
+  const [size , setSize] = useState('');
 
   const fetchProductData = async () => {
     products.map((item) => {
@@ -55,8 +56,22 @@ const Product = () => {
                 </div>
                 <p className='mt-5 text-3xl font-medium'>{currency} {productData.price}</p>
                 <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
+                <div className='flex flex-col gap-4 my-8' >
+                  <p>Select Size</p>
+                  <div className='flex gap-2'>
+                    {productData.sizes.map((item ,index)=> (
+                      <button onClick={() => setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-500' : ''}`} key={index}>{item}</button>
+                    ))}
+                  </div>
                 </div>
-
+                    <button className='bg-black text-white px-8 py-3 text-sm  active:bg-gray-700'>ADD TO CART</button>
+                    <hr className='mt-8 sm:w-4/5'/>
+                    <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+                      <p>100% original product.</p>
+                      <p>Cash on delivery is available on this product.</p>
+                      <p>Easy return and exchange policy within 7 days.</p>
+                    </div>
+                </div>
         </div>
     </div>
   ): <div className='opacity-0'></div>

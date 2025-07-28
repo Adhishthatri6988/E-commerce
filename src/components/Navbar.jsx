4df74cdc995eb2bf9b6c179f979da29bf8b81401
Adhishthatri <asunasingh2003@gmail.com>
2025-07-28 22:33:56 +0530

    orders show from dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,7 +49,7 @@ const NavBar = () => {
             <div className='absolute hidden group-hover:block dropdown-menu pt-4 right-0'>
               <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-600 rounded'>
                 <p className='cursor-pointer hover:text-black'> My Profile</p>
-                <p className='cursor-pointer hover:text-black'> Orders</p>
+                <p onClick={()=>navigate('/orders')} className='cursor-pointer hover:text-black'> Orders</p>
                 <p onClick={logout} className='cursor-pointer hover:text-black'> Logout</p>
               </div>
             </div>}

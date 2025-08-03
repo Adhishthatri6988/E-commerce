0ea1bf83857efedc891f8d94c8c8b5959cebe331
Adhishthatri <asunasingh2003@gmail.com>
2025-08-03 21:44:49 +0530

    functionality for my orders added

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,11 +1,50 @@
 import React from 'react'
-import { useContext } from 'react'
+import { useContext , useState} from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from '../components/Title';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 const Orders = () => {
 
-  const {products , currency} = useContext(ShopContext);
+  const {backendUrl , token , currency} = useContext(ShopContext);
+
+  const [orderData, setOrderData] = useState([]);
+
+
+    const loadOrderData = async () => {
+      try {
+        if(!token) {
+          return null;
+        }
+        const response = await axios.post(backendUrl+ '/api/order/userorders' , {}, {headers:{token}});
+        if(response.data.success) {
+          let allOrdersItems = [];
+          response.data.orders.map((order)=> {
+            order.items.map((item) => {
+              item['status'] = order.status;
+              item['payment']= order.payment;
+              item['paymentMethod'] = order.paymentMethod;
+              item['date'] = order.date;
+              allOrdersItems.push(item);
+            })
+          })
+          setOrderData(allOrdersItems.reverse())
+        } else {
+          console.error("Failed to fetch orders:", response.data.message);
+        }
+        
+      } catch (error) {
+        
+      }
+
+      
+    }
+
+    useEffect(()=> {
+      loadOrderData();
+    },[token])
+
   return (
     <div className='border-t pt-16'>
 
@@ -15,7 +54,7 @@ const Orders = () => {
 
       <div>
               {
-                products.slice(1,4).map((item , index) => (
+                orderData.map((item , index) => (
                   <div key={index} className='py-4 border-t border-b text-gray-700 flex flex-col md:flex-row  md:items-center md:justify-between gap-4'>
 
                     <div className='flex items-start gap-6 text-sm' >
